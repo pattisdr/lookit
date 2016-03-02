065f5168edbd2a4f08f8e57df4d1e2cfe2c36571
Chris Seto <chriskseto@gmail.com>
2016-03-02 11:42:18 -0500

    Use attributes.token

--- app/authorizers/jam-jwt.js
+++ app/authorizers/jam-jwt.js
@@ -1,12 +1,7 @@
-
 import Base from 'ember-simple-auth/authorizers/base';
 
 export default Base.extend({
-//  authorize(sessionData, block) {
-//      block('Authorization', sessionData.attributes.token);
-//  }
-    //OR
   authorize(sessionData, setHeader) {
-    setHeader('Authorization', sessionData.token);
+    setHeader('Authorization', sessionData.attributes.token);
   }
 });
