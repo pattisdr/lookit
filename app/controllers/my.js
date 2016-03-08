83a08d5e62057de3b4a550017aa8b92da24a8223
Brian J. Geiger <bgeiger@pobox.com>
2016-03-08 14:57:36 -0500

    Load default information into children edit fields

--- app/controllers/my.js
+++ app/controllers/my.js
@@ -6,17 +6,17 @@ export default Ember.Controller.extend({
     session: service('session'),
     sessionAccount: service('session-account'),
     isEditing: false,
-    
+
     actions: {
         // To add a new profile in the Children Information tab
         createProfile: function() {
-            
+
             var firstName = this.get('newFirstName');
             var birthday = new Date(this.get('newBirthday'));
             var profileId = this.get('model.username') + "." + Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5);
-            
+
             var profile = {"birthday": birthday, "firstName": firstName, "profileId": profileId};
-            
+
             if (this.get('model.profiles').length !== 0) {
                 this.get('model.profiles').pushObject(profile);
             }
@@ -24,10 +24,10 @@ export default Ember.Controller.extend({
                 this.set('model.profiles',[]);
                 this.get('model.profiles').pushObject(profile);
             }
-            
-            
+
+
             this.get('model').save();
-            
+
             // Reset input fields
             this.set('newFirstName','');
             this.set('newBirthday','');
@@ -38,12 +38,9 @@ export default Ember.Controller.extend({
         // To edit an existing profile
         editProfile: function(profile) {
             // NOTE: To set properties on a JS object (NOT Ember object), must use Ember.set
-            Ember.setProperties(profile, {'firstName': this.get('editFirstName'),'birthday': new Date(this.get('editBirthday'))});
-         
-            this.get('model').save();
+            Ember.setProperties(profile, {'firstName': profile.get('firstName'),'birthday': new Date(profile.get('birthday'))});
 
-            this.set('editFirstName','');
-            this.set('editBirthday','');
+            this.get('model').save();
             this.send('edit');
         }
     }
