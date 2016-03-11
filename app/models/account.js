96b0f611267e9876c58d9d8ab2ba2620272d3f5a
Brian J. Geiger <bgeiger@pobox.com>
2016-03-11 15:34:20 -0500

    Add demographics, except for displaying the store value when the page loads

--- app/models/account.js
+++ app/models/account.js
@@ -6,5 +6,19 @@ export default Account.extend({
     mustResetPassword: DS.attr('boolean'),
     emailPreferencesResearcherQuestions: DS.attr('boolean'),
     emailPreferencesNewStudies: DS.attr('boolean'),
-    emailPreferencesResultsPublished: DS.attr('boolean')
+    emailPreferencesResultsPublished: DS.attr('boolean'),
+    demographicsLanguagesSpokenAtHome: DS.attr('string'),
+    demographicsNumberOfChildren: DS.attr('string'),
+    demographicsChildrenBirthDates: DS.attr('string'),
+    demographicsNumberOfGuardians: DS.attr('string'),
+    demographicsNumberOfGuardiansExplanation: DS.attr('string'),
+    demographicsRaceIdentification: DS.attr('string'),
+    demographicsAge: DS.attr('string'),
+    demographicsGender: DS.attr('string'),
+    demographicsEducationLevel: DS.attr('string'),
+    demographicsSpouseEducationLevel: DS.attr('string'),
+    demographicsAnnualIncome: DS.attr('string'),
+    demographicsWillingToBeContactedForSimilarStudies: DS.attr('string'),
+    demographicsCanScheduleAnAppointment: DS.attr('string'),
+    demographicsAdditionalComments: DS.attr('string')
 });
