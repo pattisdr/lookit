59b355c29fa1939548448d7c0ad657555720634b
Chris Seto <chriskseto@gmail.com>
2016-03-12 19:33:44 -0500

    Small validation update

--- app/components/jam-create.js
+++ app/components/jam-create.js
@@ -1,4 +1,4 @@
-import Ember from 'ember';
+import Em from 'ember';
 import ENV from 'lookit-base/config/environment';
 
 export default Ember.Component.extend({
@@ -8,6 +8,12 @@ export default Ember.Component.extend({
   username: null,
   password: null,
     email: null,
+
+  maySubmit: function() {
+      return ['username', 'password', 'email']
+          .map(k => this.get(k))
+          .reduce((acc, val) => acc && val);
+  }.property('username', 'password', 'email'),
 
   actions: {
     createAccount() {
