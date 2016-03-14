cbfe65306be8875fe7980a031a8a0711ac59f0fd
Lauren Barker <leb2dg@virginia.edu>
2016-03-14 11:02:47 -0400

    save all family categories in demographic survey

---
+++ tests/unit/helpers/include-test.js
@@ -0,0 +1,10 @@
+import { include } from '../../../helpers/include';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | include');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = include([42]);
+  assert.ok(result);
+});
