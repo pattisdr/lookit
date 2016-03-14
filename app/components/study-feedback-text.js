bb28a4f92981ea484c8e43c98cab4a40cce05bfc
Andy Boughton <abought@gmail.com>
2016-03-14 13:10:05 -0400

    Fetch the first name associated with a particular account

--- app/components/study-feedback-text.js
+++ app/components/study-feedback-text.js
@@ -1,10 +1,37 @@
 import Ember from 'ember';
 
+var PLACEHOLDER = 'Loading...';
+
 export default Ember.Component.extend({
     session: null,
 
     showFeedback: false,
     feedbackText: 'Show Feedback',
+
+    store: Ember.inject.service(),
+
+    _profileName: PLACEHOLDER,  // Show... something... until data has fully loaded
+    profileName: Ember.computed('_profileName', function() {
+        return this.get('_profileName');
+    }),
+
+    /*
+        On component creation, fetch the username associated with a specific profile
+     */
+    init() {
+        this._super(...arguments);
+
+        let session = this.get('session');
+        let profileId = session.get('profileId');
+
+        // Fetch name associated with profile, then update page
+        this.get('store').queryRecord('account', {'filter[profiles.profileId]': profileId})
+            .then((res) => {
+                let profile = res[0].profileById(profileId);
+                this.set('_profileName', profile.firstName);
+            });
+    },
+
     actions: {
         toggleFeedback: function() {
             this.toggleProperty('showFeedback');
