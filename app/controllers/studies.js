47605eddb372e167ced2ed845ba0f64cc3280bd3
Andy Boughton <abought@gmail.com>
2016-03-14 10:59:37 -0400

    Simplify markup/ remove dead code

--- app/controllers/studies.js
+++ app/controllers/studies.js
@@ -3,15 +3,5 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
     session: Ember.inject.service('session'),
-    sessionAccount: Ember.inject.service('session-account'),
-
-    loggedIn: Ember.computed(function () {
-        return !!this.get('sessionAccount').account;  // TODO: Can we just use session.isAuthenticated ?
-    }),
-
-    actions: {
-        updateHeader: function (header) {
-            this.set('header', header);
-        }
-    }
+    sessionAccount: Ember.inject.service('session-account')
 });
