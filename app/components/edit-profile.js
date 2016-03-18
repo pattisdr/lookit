1abe9ef848aac713fec87e762a273b96b4cb2935
Sam Chrisinger <s.chrisinger@gmail.com>
2016-03-18 16:07:45 -0400

    Consolidate child editing logic

--- app/components/edit-profile.js
+++ app/components/edit-profile.js
@@ -1,52 +1,36 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-    firstName: Ember.computed(function() {
-        return this.profile.firstName;
-    }),
-    birthday: Ember.computed(function() {
-        return this.profile.birthday;
-    }),
-    gender: Ember.computed(function() {
-        return this.profile.gender || null;
-    }),
-    ageAtBirth: Ember.computed(function() {
-        return this.profile.ageAtBirth || null;
-    }),
-    isDirty: Ember.computed('firstName', 'birthday', 'gender', 'ageAtBirth', function() {
-        if (this.get('firstName') !== this.get('profile.firstName') || this.get('birthday') !== this.get('profile.birthday') || this.get('gender') !== this.get('profile.gender') || this.get('ageAtBirth') !== this.get('profile.ageAtBirth')) {
-            return true;
-        }
-        return false;
-    }),
-    isValid: Ember.computed('firstName', 'birthday', 'gender', 'ageAtBirth', function() {
-        if (!Ember.isEmpty(this.get('firstName')) && !Ember.isEmpty(this.get('birthday')) && !Ember.isEmpty(this.get('gender')) && !Ember.isEmpty(this.get('ageAtBirth'))) {
+    profile: null,
+    account: null,
+    isDirty: Ember.computed.alias('profile.hasDirtyAttributes'),
+    isValid: Ember.computed('profile.firstName', 'profile.birthday', 'profile.gender', 'profile.ageAtBirth', function() {
+        if (!Ember.isEmpty(this.get('profile.firstName')) && !Ember.isEmpty(this.get('profile.birthday')) && !Ember.isEmpty(this.get('profile.gender')) && !Ember.isEmpty(this.get('profile.ageAtBirth'))) {
             return true;
         }
         return false;
     }),
     actions: {
         save: function(profile) {
-            Ember.setProperties(profile, {'firstName': this.get('firstName'),'birthday': new Date(this.get('birthday')), 'gender': this.get('gender'), 'ageAtBirth': this.get('ageAtBirth')});
-            this.get('model').save().then(() => {
-                this.toast.info('Child profile updated successfully.');
+            var account = this.get('account');
+            var verb = 'updated';
+            if (account.get('profiles').indexOf(profile) === -1) {
+                account.get('profiles').unshiftObject(profile);
+                verb = 'added';
+                this.get('onAdd')();
+            }
+
+            this.get('account').save().then(() => {
+                profile.save();
+                this.toast.info(`Child profile ${verb} successfully.`);
             });
         },
         cancel: function(profile) {
-            this.set('firstName', profile.get('firstName'));
-            this.set('birthday', profile.get('birthday'));
-            this.set('gender', profile.get('gender'));
-            this.set('ageAtBirth', profile.get('ageAtBirth'));
+            profile.rollbackAttributes();
         },
         delete: function(profile) {
-            if (this.get('model.profiles').length !== 0) {
-                this.get('model.profiles').removeObject(profile);
-            }
-            else {
-                console.log('No profiles to delete.');
-            }
-
-            this.get('model').save().then(() => {
+            profile.set('deleted', true);
+            this.get('account').save().then(() => {
                 this.toast.info('Child profile deleted successfully.');
             });
         }
--- app/controllers/my/children.js
+++ app/controllers/my/children.js
@@ -5,9 +5,24 @@ const { service } = Ember.inject;
 export default Ember.Controller.extend({
     session: service('session'),
     sessionAccount: service('session-account'),
-    toast: Ember.inject.service(),
-    isValid: Ember.computed('newFirstName', 'newBirthday', 'newGender', 'newAgeAtBirth', function() {
-        if (!Ember.isEmpty(this.get('newFirstName')) && !Ember.isEmpty(this.get('newBirthday')) && !Ember.isEmpty(this.get('newGender')) && !Ember.isEmpty(this.get('newAgeAtBirth'))) {
+    store: service(),
+    toast: service(),
+
+    addingNew: false,
+    notAddingNew: Ember.computed.not('addingNew'),
+    profile: null,
+    newProfile: function() {
+        return this.get('store').createRecord('profile', {
+            profileId: this.get('model').generateProfileId(),
+            deleted: false
+        });
+    },
+
+    onModelChange: Ember.observer('model', function() {
+        this.set('profile', this.newProfile());
+    }),
+    isValid: Ember.computed('profile.firstName', 'profile.birthday', 'profile.gender', 'profile.ageAtBirth', function() {
+        if (!Ember.isEmpty(this.get('profile.firstName')) && !Ember.isEmpty(this.get('profile.birthday')) && !Ember.isEmpty(this.get('profile.gender')) && !Ember.isEmpty(this.ge('profile.ageAtBirth'))) {
             return true;
         }
         return false;
@@ -21,42 +36,13 @@ export default Ember.Controller.extend({
         'Under 24 Weeks',
         'Over 24 Weeks'
     ],
-    newAgeAtBirth: null,
     actions: {
-        // To add a new profile in the Children Information tab
-        createProfile: function() {
-            $('.collapse').collapse("hide");
-            var firstName = this.get('newFirstName');
-            var birthday = new Date(this.get('newBirthday'));
-            var profileId = this.get('model.username') + "." + Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5);
-            var gender = this.get('newGender');
-            var ageAtBirth = this.get('newAgeAtBirth');
-
-            var profile = {"birthday": birthday, "firstName": firstName, "profileId": profileId, "gender": gender, "ageAtBirth": ageAtBirth};
-
-            if (this.get('model.profiles').length !== 0) {
-                this.get('model.profiles').pushObject(profile);
-            }
-            else {
-                this.set('model.profiles',[]);
-                this.get('model.profiles').pushObject(profile);
-            }
-
-            this.get('model').save().then(() => {
-                this.toast.info('Profile created successfully.');
-            });
-
-            // Reset input fields
-            this.set('newFirstName','');
-            this.set('newBirthday','');
-            this.set('newGender', null);
-            this.set('newAgeAtBirth', null);
+        profileAdded () {
+            this.set('profile', this.newProfile());
+            this.toggleProperty('addingNew');
         },
-        cancel: function() {
-            this.set('newFirstName','');
-            this.set('newBirthday','');
-            this.set('newGender', null);
-            this.set('newAgeAtBirth', null);
+        toggleAddingNew () {
+            this.toggleProperty('addingNew');
         }
     }
 });
