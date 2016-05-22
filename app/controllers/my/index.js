199c5943add3c44e89f23b0231d4a5bcc9c45a60
Sam Chrisinger <s.chrisinger@gmail.com>
2016-05-22 13:28:56 -0400

    Don't require old password for password changes

--- app/controllers/my/index.js
+++ app/controllers/my/index.js
@@ -1,10 +1,10 @@
+/* global dcodeIO */
 import Ember from 'ember';
 
-const {bcrypt} = dcodeIO; // jshint ignore: line
+const {bcrypt} = dcodeIO;
 
 export default Ember.Controller.extend({
     _error: null,
-    oldPass: null,
     newPass: null,
     confirmPass: null,
     toast: Ember.inject.service(),
@@ -14,11 +14,7 @@ export default Ember.Controller.extend({
         return this.get('newPass') === this.get('confirmPass');
     }.property('newPass', 'confirmPass'),
 
-    canUpdate: function() {
-        return this.get('passMatch') && ['oldPass', 'newPass', 'confirmPass']
-            .map(attr => this.get(attr))
-            .reduce((acc, val) => !Ember.isEmpty(val) && acc, true);
-    }.property('passMatch', 'oldPass', 'newPass', 'confirmPass'),
+    canUpdate: Ember.computed.and('newPass', 'confirmPass', 'passMatch'),
 
     actions: {
         save() {
@@ -30,15 +26,9 @@ export default Ember.Controller.extend({
             this.get('model').rollbackAttributes();
         },
         updatePassword() {
-            if (!bcrypt.compareSync(this.get('oldPass'), this.get('model.password'))) {
-                this.set('_error', 'Incorrect password');
-                return;
-            }
-
             this.set('model.password', bcrypt.hashSync(this.get('newPass'), 10).replace('$2a$', '$2b$'));
             this.get('model').save().then(() => {
                 this.set('_error', null);
-                this.set('oldPass', null);
                 this.set('newPass', null);
                 this.set('confirmPass', null);
                 this.toast.info('Password updated successfully.');
