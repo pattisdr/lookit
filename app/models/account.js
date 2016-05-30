c4c035bf22a59c1b22fbe5ad237e8fc6106ae6ac
Sam Chrisinger <s.chrisinger@gmail.com>
2016-05-30 14:56:25 -0400

    Add suppression management support

--- app/models/account.js
+++ app/models/account.js
@@ -9,7 +9,7 @@ const DEMOGRAPHIC_FIELDS = ['demographicsLanguagesSpokenAtHome', 'demographicsNu
 const ASM_MAPPING = {
     nextSession: {
         label: 'Next Session',
-        descrtiption: 'It\'s time for another session of a study we are currently participating in',
+        description: 'It\'s time for another session of a study we are currently participating in',
         id: '911'
     },
     newStudies: {
@@ -21,6 +21,11 @@ const ASM_MAPPING = {
         label: 'Results Published',
         description: 'The results of a study we participated in are published',
         id: '915'
+    },
+    optOut: {
+        label: 'Opt Out',
+        description: 'A researcher may also email you personally if we have questions about your responses (for example, if you report two different birthdates for a child and we\'re not sure whether he\'s 3 or 5) or if you report a technical problem. Check here to opt out:',
+        id: '1117'
     }
 };
 
@@ -47,45 +52,55 @@ export default Account.extend({
     hasCompletedSurvey: Ember.computed.or(...DEMOGRAPHIC_FIELDS),
 
     suppressionsUrl: Ember.computed('email', function() {
-	var email = this.get('email');
         var groups = Object.keys(ASM_MAPPING).map(key => {
             return `group[]=${ASM_MAPPING[key].id}`;
         }).join('&');
-	return `${config.JAMDB.url}/v1/id/collections/${config.JAMDB.namespace}.accounts/user?email=${email}&${groups}`;
+        return `${config.JAMDB.url}/v1/id/collections/${config.JAMDB.namespace}.accounts/user?id=${this.get('id')}&${groups}`;
     }),
     getSuppressions() {
-	var url = this.get('suppresionsUrl');
-	var adapter = this.store.adapterFor('application');
-	return adapter.ajax(
-	    url,
-	    'GET',
-	    adapter.ajaxOptions(
-		url,
-		'GET'
-	    )
-	).then(res => {
-	    debugger;
-	});
+        var url = this.get('suppressionsUrl');
+        var adapter = this.store.adapterFor('application');
+        return adapter.ajax(
+            url,
+            'GET',
+            adapter.ajaxOptions(
+                url,
+                'GET'
+            )
+        ).then(res => {
+            return Object.keys(ASM_MAPPING).map(key => {
+                var group = ASM_MAPPING[key];
+                group.attr = key;
+                group.subscribed = !res.data.attributes[group.id];
+                if (key === 'optOut') {
+                    group.subscribed = !group.subscribed;
+                }
+                return group;
+            });
+        });
     },
     setSuppressions(suppressionsHash) {
-	var url = this.get('suppresionsUrl');
-	var adapter = this.store.adapterFor('application');
-	return adapter.ajax(
-	    url,
-	    'PATCH',
-	    adapter.ajaxOptions(
-		url,
-		'PATCH',
-		{
-		    data: {
-			type: "supressions",
-			attributes: suppressionsHash
-		    }
-		}
-	    )
-	).then(res => {
-	    debugger;
-	});
-
+        suppressionsHash.id = this.get('id');
+        suppressionsHash[ASM_MAPPING.optOut.id] = !suppressionsHash[ASM_MAPPING.optOut.id];
+        var url = this.get('suppressionsUrl');
+        var adapter = this.store.adapterFor('application');
+        return adapter.ajax(
+            url,
+            'PATCH', {
+                data: {
+                    data: {
+                        type: "supressions",
+                        attributes: suppressionsHash
+                    }
+                }
+            }
+        ).then(res => {
+            return Object.keys(ASM_MAPPING).map(key => {
+                var group = JSON.parse(JSON.stringify(ASM_MAPPING[key]));
+                group.attr = key;
+                group.subscribed = !res.data.attributes[group.id];
+                return group;
+            });
+        });
     }
 });
