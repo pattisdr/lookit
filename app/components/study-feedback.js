aef72ced15bbf825e14e2bf872d3a5d69b380047
Sam Chrisinger <s.chrisinger@gmail.com>
2016-06-07 13:02:29 -0400

    Fix study eligibilty on study detail view

--- app/components/study-feedback.js
+++ app/components/study-feedback.js
@@ -1,20 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-    ageRange: Ember.computed('experiment', function() {
-	let {
-	    eligibilityMinAge,
-	    eligibilityMaxAge
-	} = this.getProperties('eligibilityMinAge', 'eligibilityMaxAge');
-
-	if (eligibilityMinAge && eligibilityMaxAge) {
-	    return `between ${eligibilityMinAge} and ${eligibilityMaxAge}`;
-	} else if (eligibilityMinAge) {
-	    return `${eligibilityMinAge} and up`;
-	} else if (eligibilityMaxAge) {
-	    return `not older than ${eligibilityMaxAge}`;
-	} else {
-	    return 'any';
-	}
-    })
 });
