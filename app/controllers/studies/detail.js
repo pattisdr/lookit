727f3d185557b2a4dbe85a2c59ce46c9fd730685
Sam Chrisinger <s.chrisinger@gmail.com>
2016-08-05 10:54:32 -0400

    Warn if child is outside of eligibility age range

--- app/controllers/studies/detail.js
+++ app/controllers/studies/detail.js
@@ -1,22 +1,46 @@
 import Ember from 'ember';
+import moment from 'moment';
 
 export default Ember.Controller.extend({
     session: Ember.inject.service(),
     sessionAccount: Ember.inject.service(),
+    account: Ember.computed.alias('sessionAccount.account'),
     selectedChildId: null,
+    selectedChild: Ember.computed('selectedChildId', function() {
+        let account = this.get('sessionAccount').get('account');
+        return account.profileById(this.get('selectedChildId'));
+    }),
+
+    isAgeEligible: Ember.computed('selectedChild', function() {
+        let child = this.get('selectedChild');
+        if (!child) {
+            return true;
+        }
+
+        let experiment = this.get('model');
+        let {
+            eligibilityMinAge,
+            eligibilityMaxAge
+        } = experiment.getProperties('eligibilityMinAge', 'eligibilityMaxAge');
+        let [minNumber, minUnit] = eligibilityMinAge.split(' ');
+        let minDays = moment.duration(parseFloat(minNumber), minUnit).asDays();
+        let [maxNumber, maxUnit] = eligibilityMaxAge.split(' ');
+        let maxDays = moment.duration(parseFloat(maxNumber), maxUnit).asDays();
+
+        var diff = moment().diff(child.get('birthday'), 'days');
+        return minDays <= diff && diff <= maxDays;
+    }),
 
     route: function() {
-        return `${Ember.getOwner(this).lookup('router:main').get('currentPath')}:${this.get('model.experiment.id')}`;
+        return `${Ember.getOwner(this).lookup('router:main').get('currentPath')}:${this.get('model.id')}`;
     }.property('model'),
 
     actions: {
-        pickChild: function() {
-            var account = this.get('sessionAccount').get('account');
-            var profile = account.profileById(this.get('selectedChildId'));
-            // TODO consolidate
+        pickChild() {
+            let profile = this.get('selectedChild');
             this.get('sessionAccount').setProfile(profile);
             this.get('session').set('data.profile', profile);
-            this.transitionToRoute('participate', this.get('model.experiment.id'));
+            this.transitionToRoute('participate', this.get('model.id'));
         }
     }
 });
--- app/routes/studies/detail.js
+++ app/routes/studies/detail.js
@@ -1,12 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-    sessionAccount: Ember.inject.service('session-account'),
     model(params) {
-        return Ember.RSVP.hash({
-            experiment: this.store.findRecord('experiment', params.experiment_id),
-            account: this.get('sessionAccount').loadCurrentUser()  // TODO: Is this necessary (see application route)
-        });
+        return this.store.findRecord('experiment', params.experiment_id);
     },
     resetController: function(controller, isExiting) {
         if (isExiting) {
