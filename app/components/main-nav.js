3cdfd1a7ed30c3e9488cdff5731c998d0f9f05e2
Sam Chrisinger <s.chrisinger@gmail.com>
2016-08-10 11:49:30 -0400

    Fallback on username if account name is missing

--- app/components/main-nav.js
+++ app/components/main-nav.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
     session: Ember.inject.service('session'),
     sessionAccount: Ember.inject.service('session-account'),
-    accountName: Ember.computed.alias('sessionAccount.account.name'),
+    accountName: Ember.computed.or('sessionAccount.account.name', 'sessionAccount.account.username'),
     actions: {
         logout() {
             Ember.getOwner(this).lookup('controller:application').send('logout');
