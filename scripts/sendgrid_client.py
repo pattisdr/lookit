23e4562901fa682afe04357c759cde4a07cbba36
Sam Chrisinger <s.chrisinger@gmail.com>
2016-08-15 15:38:20 -0400

    Add tests for SendGrid examples

--- scripts/sendgrid_client.py
+++ scripts/sendgrid_client.py
@@ -1,5 +1,7 @@
+import argparse
 import sendgrid
 import json
+import pprint
 
 import conf
 
@@ -45,7 +47,7 @@ class SendGrid(object):
         ) or []
 
     def unsubscribe_from(self, group, email):
-        return self.batch_unsunscribe_from(group, [email])
+        return self.batch_unsubscribe_from(group, [email])
 
     def batch_unsubscribe_from(self, group, emails):
         return json.loads(
@@ -55,11 +57,10 @@ class SendGrid(object):
         )
 
     def subscribe_to(self, group, email):
-        return json.loads(
-            self.sg.client.asm.groups._(group['id']).suppressions._(
-                email
-            ).delete().response_body
-        )
+        self.sg.client.asm.groups._(group['id']).suppressions._(
+            email
+        ).delete()
+        return None
 
     def send_email_to(self, email, subject, body, group_id=None):
         message = sendgrid.Mail()
@@ -72,3 +73,46 @@ class SendGrid(object):
             message.set_asm_group_id(group_id)
         status, msg = self.smtp.send(message)
         print(status, msg)
+
+
+def test(recipient_email, subscribe=True, unsubscribe=False):
+    sg = SendGrid()
+    groups = sg.groups()
+    for _, group in groups.items():
+        print "Emails currently in the unsubscribe group '{}':".format(group['name'])  # noqa
+        print "{}".format(', '.join(sg.unsubscribes_for(group)))
+        print "-------------------"
+        print "{}ubscribing {}".format('S' if not unsubscribe else 'Uns', recipient_email)  # noqa
+        if not unsubscribe:
+            sg.subscribe_to(group, recipient_email)
+        else:
+            sg.unsubscribe_from(group, recipient_email)
+        print "-------------------"
+        print "Emails now in the unsubscribe group:"
+        print "{}".format(', '.join(sg.unsubscribes_for(group)))
+        print "-------------------"
+        print "Sending a test email to {} with group_id {}".format(
+            recipient_email,
+            group['id']
+        )
+        sg.send_email_to(
+            recipient_email,
+            'A test',
+            'This is a test',
+            group_id=group['id']
+        )
+        print "*******************"
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-r', '--recipient', type=str, required=True,
+                        help='A recipient email')
+    parser.add_argument('-s', '--subscribe', type=bool, default=True,
+                        help='Subscribe to ASM groups?')
+    parser.add_argument('-u', '--unsubscribe', type=bool, default=False,
+                        help='Unsubscribe from ASM groups?')
+    args = parser.parse_args()
+    test(args.recipient,
+         subscribe=args.subscribe,
+         unsubscribe=args.unsubscribe)
