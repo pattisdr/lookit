5296e24f3a1943c5d3402d7c80138da3d8ea1d1d
Andy Boughton <abought@gmail.com>
2016-10-07 10:00:21 -0400

    Continue refactoring beforeModel hook to avoid issue....
    This version seems to hang on invalid token. Odd.
    
    [ci skip]
    [#LEI-233]

--- app/routes/my.js
+++ app/routes/my.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
+import esaConfig from 'ember-simple-auth/configuration';
+
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
     session: Ember.inject.service(),
     sessionAccount: Ember.inject.service(),
@@ -9,14 +11,21 @@ export default Ember.Route.extend(AuthenticatedRouteMixin, {
         return this.get('sessionAccount').load();
     },
     beforeModel(transition) {
-        if (transition.queryParams.access_token) {
+        let token = transition.queryParams.access_token;
+        if (this.get('session.isAuthenticated') || !token) {
+            // Either user is authenticated, or no token is present- either way, auth mixin should act normally
+            return this._super(...arguments);
+        } else {
+            // User is unauthenticated, but a token is present- we must bypass ESA so that user is automatically
+            //   logged in but remains on this page. (avoid race conditions between sessionAuthenticated events and super call)
+            transition.abort();
+            this.set('session.attemptedTransition', transition);
             return this.get('session')
-                .authenticate('authenticator:jam-jwt', {}, transition.queryParams.access_token)
-                .then(() => {
-                    window.location.hash = '';
-                    return this._super(...arguments);
-                });
+                .authenticate('authenticator:jam-jwt', {}, token)
+                .then(() => window.location.hash = '')
+                .catch(() => this.transitionTo(esaConfig.authenticationRoute));
+
+
         }
-        return this._super(...arguments);
     }
 });
