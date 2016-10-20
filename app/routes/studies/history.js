c8d80c755361744ab2f4b0120f71e6d0fad1f0ed
Andy Boughton <abought@gmail.com>
2016-10-20 16:59:45 -0400

    Slightly refactor model hook for readability
    Long term this route needs to be restructured- it's not scalable and won't handle pagination very well
    
    [ci skip]
    [#LEI-266]

--- app/routes/studies/history.js
+++ app/routes/studies/history.js
@@ -5,32 +5,32 @@ import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-rout
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
 
     model( /*params*/ ) {
+        // In the future, routes should be restructured to better support more experiments
         let Experiment = this.store.modelFor('experiment');
+        let experiments = this.store.query('experiment', {
+            q: `state:${Experiment.prototype.ACTIVE} OR state:${Experiment.prototype.ARCHIVED}`
+        });
+
+        let sessionPromises = [];
+        let experimentSessions = [];
 
         // Return the result of a query for all experiments
-        return this.store.query('experiment', {
-            q: `state:${Experiment.prototype.ACTIVE} OR state:${Experiment.prototype.ARCHIVED}`
-        }).then((experiments) => {
-            let promises = [];
-            let experimentSessions = [];
-
+        return experiments.then((experiments) => {
             experiments.forEach((experiment) => {
-                promises.push( // Endpoint only returns sessions that the user has permissions to see (assumption: only their own)
-                    this.store.query(experiment.get('sessionCollectionId'), {
-                        'filter[completed]': 1
-                    }).then((sessions) => {
-                        if (sessions.get('length') > 0) {
-                            experimentSessions.push({
-                                experiment: experiment,
-                                sessions: sessions
-                            });
-                        }
-                    }));
+                // Endpoint only returns sessions that the user has permissions to see (assumption: for a Jam user, this means only their own sessions)
+                let foundSessions = this.store.query(experiment.get('sessionCollectionId'), {
+                    'filter[completed]': 1
+                }).then((sessions) => {
+                    if (sessions.get('length') > 0) {
+                        experimentSessions.push({
+                            experiment: experiment,
+                            sessions: sessions
+                        });
+                    }
+                });
+                sessionPromises.push(foundSessions);
             });
-
-            return Ember.RSVP.all(promises).then(() => {
-                return experimentSessions;
-            });
+            return Ember.RSVP.all(sessionPromises).then(() => experimentSessions);
         });
     }
 });
